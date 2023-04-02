05e873a720762255da52179b0d13e04ee30945fd
Tuan Anh <tunanguyenlc@gmail.com>
2023-04-02 23:42:36 +0700

    Reactjs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import Th3 from './Bai9_TH3/Th3'
+import Th4 from './Bai9_TH4/Th4';
+import Bai8_BT1 from './Bai8_BT1/Bai8_BT1';
+import Bai8_BT2 from './Bai8_BT2/Bai8_BT2';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1> Bai8_BT1</h1>
+      <Bai8_BT1></Bai8_BT1>
+
+      <h1>Bai8_BT2</h1>
+      <Bai8_BT2></Bai8_BT2>
+
+      <h1>Bai9_Thuc Hanh 3</h1>
+      <Th3></Th3>
+
+      <h1>Bai9_Thuc Hanh 4</h1>
+      <Th4></Th4>
     </div>
   );
+
 }
 
 export default App;
